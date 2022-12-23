1a8c70cbc8f53caf36653c76b3eec86bac651481
SelligtomGit86 <thomas.delafaye@serli.com>
2022-12-23 11:15:08 +0100

    choose canva when you add a new manifest

--- src/components/AdnoViewer/ViewerAnnotationCards/OneCardView/OneCardFullView.js
+++ src/components/AdnoViewer/ViewerAnnotationCards/OneCardView/OneCardFullView.js
@@ -50,9 +50,9 @@ class OneCardFullView extends Component {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                 </button>
                             </div>
-                            <div class="card-body over-hidden">
-                                <div class="card-text">
-                                    <div id="editorJS" className="p-3"></div>
+                            <div className="card-body over-hidden">
+                                <div className="card-text">
+                                    <div id="editorJS"></div>
                                 </div>
                             </div>
 
