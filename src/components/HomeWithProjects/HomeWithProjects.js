232ce7064933b41a9a1b0cabbbd08e1ca91f2331
SelligtomGit86 <thomas.delafaye@serli.com>
2023-02-14 10:54:32 +0100

    :ambulance: fix url issue : error when adding an image

--- src/components/HomeWithProjects/HomeWithProjects.js
+++ src/components/HomeWithProjects/HomeWithProjects.js
@@ -9,7 +9,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Swal from "sweetalert2";
 
 // Import utils
-import { buildJsonProjectWithManifest, generateUUID, getAllProjectsFromLS, insertInLS, isValidUrl } from "../../Utils/utils";
+import { buildJsonProjectWithManifest, generateUUID, getAllProjectsFromLS, get_url_extension, insertInLS, isValidUrl } from "../../Utils/utils";
 
 // Import components
 import ImportProject from "../ImportProject/ImportProject";
@@ -42,103 +42,133 @@ class HomeWithProjects extends Component {
             // Also, we check if the url is not empty, not undefined and not null
             if (this.state.adno_image_url) {
 
-                // Then, we check if the given URL is valid
-                // Finally, we check if the URL is reachable
-                if (isValidUrl(this.state.adno_image_url)) {
-
+                // We check if the url contains an image
+                if (get_url_extension(this.state.adno_image_url) === "png" || get_url_extension(this.state.adno_image_url) === "jpg" || get_url_extension(this.state.adno_image_url) === "jpeg") {
                     fetch(this.state.adno_image_url)
                         .then(res => {
-                            if (res.ok) {
-                                return res.text()
-                            } else {
-                                throw new Error(`Error ${res.status}`)
-                            }
-                        })
-                        .then((data) => {
-
-                            // First, we check if the file looks like an ADNO project
-                            // If an adno project is found then, import it directly
-                            let manifest = JSON.parse(data)
-
-                            if (manifest.format && manifest.format === "Adno") {
-                                Swal.fire({
-                                    title: "Projet ADNO détecté, voulez-vous l'importer ?",
-                                    showCancelButton: true,
-                                    showConfirmButton: true,
-                                    confirmButtonText: 'OK',
-                                    cancelButtonText: "Annuler",
-                                    icon: 'info'
-                                })
-                                    .then((result) => {
-                                        if (result.isConfirmed) {
-                                            let projectID = generateUUID();
-
-                                            let project = buildJsonProjectWithManifest(projectID, manifest.label, manifest.subject, manifest.source)
-
-                                            // Création du projet dans le localStorage
-                                            insertInLS(projectID, JSON.stringify(project))
-
-                                            // Insertion de l'ID du projet créé dans le tableau des projets
-                                            let projects = JSON.parse(localStorage.getItem("adno_projects"))
-                                            projects.push(projectID)
-
-                                            // Add the current project's id to the projects's list.
-                                            insertInLS("adno_projects", JSON.stringify(projects))
-
-                                            // Insert in LS an array for the annotations linked to this project
-                                            insertInLS(`${projectID}_annotations`, JSON.stringify(manifest.first.items))
-
-                                            Swal.fire({
-                                                title: "Projet importé avec succès",
-                                                showCancelButton: false,
-                                                showConfirmButton: true,
-                                                confirmButtonText: 'OK',
-                                                icon: 'success'
-                                            })
-                                                .then((result) => {
-                                                    if (result.isConfirmed) {
-                                                        this.props.history.push(`/project/${projectID}/edit`)
-                                                    }
-                                                })
-
-
-                                        }
-                                    })
-
-
-                            } else {
-                                // If the URL doesn't look like to an adno project just import it from the New Project Page
+                            if (res.status == 200 || res.status == 201) {
                                 insertInLS("adno_image_url", this.state.adno_image_url)
-
-                                this.props.history.push("/new");
+                                this.props.history.push("/new")
+                            } else {
+                                throw new Error("Impossible d'accéder à ce fichier")
                             }
                         })
-                        .catch(error => {
-                            console.error("erreur détectée : ", error)
+                        .catch(err => {
                             Swal.fire({
-                                title: `Erreur - Manifest ou image introuvable`,
-                                showCancelButton: true,
-                                showConfirmButton: false,
-                                cancelButtonText: 'OK',
-                                icon: 'warning',
+                                title: `Erreur détectée : ${err.message}`,
+                                showCancelButton: false,
+                                showConfirmButton: true,
+                                confirmButtonText: 'OK',
+                                icon: 'error'
                             })
+                                .then((result) => {
+                                    if (result.isConfirmed) {
+                                        this.props.history.push("/")
+                                    }
+                                })
                         })
-
-
-
-
-
-
-
                 } else {
-                    Swal.fire({
-                        title: "L'URL renseignée n'est pas valide !",
-                        showCancelButton: true,
-                        showConfirmButton: false,
-                        cancelButtonText: 'OK',
-                        icon: 'warning',
-                    })
+
+
+
+
+                    // Then, we check if the given URL is valid
+                    // Finally, we check if the URL is reachable
+                    if (isValidUrl(this.state.adno_image_url)) {
+
+                        fetch(this.state.adno_image_url)
+                            .then(res => {
+                                if (res.ok) {
+                                    return res.text()
+                                } else {
+                                    throw new Error(`Error ${res.status}`)
+                                }
+                            })
+                            .then((data) => {
+                                // First, we check if the file looks like an ADNO project
+                                // If an adno project is found then, import it directly
+                                let manifest = JSON.parse(data)
+
+                                if (manifest.format && manifest.format === "Adno") {
+                                    Swal.fire({
+                                        title: "Projet ADNO détecté, voulez-vous l'importer ?",
+                                        showCancelButton: true,
+                                        showConfirmButton: true,
+                                        confirmButtonText: 'OK',
+                                        cancelButtonText: "Annuler",
+                                        icon: 'info'
+                                    })
+                                        .then((result) => {
+                                            if (result.isConfirmed) {
+                                                let projectID = generateUUID();
+
+                                                let project = buildJsonProjectWithManifest(projectID, manifest.label, manifest.subject, manifest.source)
+
+                                                // Création du projet dans le localStorage
+                                                insertInLS(projectID, JSON.stringify(project))
+
+                                                // Insertion de l'ID du projet créé dans le tableau des projets
+                                                let projects = JSON.parse(localStorage.getItem("adno_projects"))
+                                                projects.push(projectID)
+
+                                                // Add the current project's id to the projects's list.
+                                                insertInLS("adno_projects", JSON.stringify(projects))
+
+                                                // Insert in LS an array for the annotations linked to this project
+                                                insertInLS(`${projectID}_annotations`, JSON.stringify(manifest.first.items))
+
+                                                Swal.fire({
+                                                    title: "Projet importé avec succès",
+                                                    showCancelButton: false,
+                                                    showConfirmButton: true,
+                                                    confirmButtonText: 'OK',
+                                                    icon: 'success'
+                                                })
+                                                    .then((result) => {
+                                                        if (result.isConfirmed) {
+                                                            this.props.history.push(`/project/${projectID}/edit`)
+                                                        }
+                                                    })
+
+
+                                            }
+                                        })
+
+                                } else {
+                                    // If the URL doesn't look like to an adno project just import it from the New Project Page
+                                    insertInLS("adno_image_url", this.state.adno_image_url)
+
+                                    this.props.history.push("/new");
+                                }
+                            })
+                            .catch(error => {
+                                console.error("erreur détectée : ", error)
+                                Swal.fire({
+                                    title: `Erreur - Manifest ou image introuvable`,
+                                    showCancelButton: true,
+                                    showConfirmButton: false,
+                                    cancelButtonText: 'OK',
+                                    icon: 'warning',
+                                })
+                            })
+
+
+
+
+
+
+
+                    } else {
+                        Swal.fire({
+                            title: "L'URL renseignée n'est pas valide !",
+                            showCancelButton: true,
+                            showConfirmButton: false,
+                            cancelButtonText: 'OK',
+                            icon: 'warning',
+                        })
+                    }
                 }
+
 
             } else {
                 // Display a warning popup if the URL is not filled
@@ -174,7 +204,7 @@ class HomeWithProjects extends Component {
                         <div className="input-group mb-3 add_url">
                             <span className="input-group-text" id="basic-addon1"> <FontAwesomeIcon icon={faLink} /> URL</span>
                             <input type="text" id="adno_image_url_2" className="input input-bordered input-primary w-full" value={this.state.adno_image_url} onChange={(e) => this.setState({ adno_image_url: e.target.value })}
-                                placeholder="https://free.iiifhosting.com/iiif/1c8d49343676a04fffcd92979c02e9394e48bac96f590fffbadffc9133cd06b9/info.json" />
+                                placeholder=" https://iiif.emf.fr/iiif/3/saint-savin.jpg/info.json" />
                         </div>
 
                         <div className="tooltip" data-tip="Créer un nouveau projet">
