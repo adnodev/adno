66d0c5d6a1cf7602b868020a83569717fb4304c4
SelligtomGit86 <thomas.delafaye@serli.com>
2023-04-11 15:10:17 +0200

    :globe_with_meridians: Translate texts on component 'ImportProject' add support of EN and FR languages

--- src/components/ImportProject/ImportProject.js
+++ src/components/ImportProject/ImportProject.js
@@ -3,7 +3,7 @@ import { withRouter } from "react-router";
 
 // Import FontAwesome and icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCancel, faCheck, faCheckCircle, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { faCancel, faCheckCircle, faUpload } from "@fortawesome/free-solid-svg-icons";
 
 // Import popup alerts
 import Swal from "sweetalert2";
@@ -13,6 +13,7 @@ import { importProjectJsonFile } from "../../Utils/utils";
 
 // Import CSS
 import "./ImportProject.css";
+import { withTranslation } from "react-i18next";
 
 class ImportProject extends Component {
     constructor(props) {
@@ -20,24 +21,24 @@ class ImportProject extends Component {
         this.state = {
             isimporting: false,
             loadedProject: "",
-            labelImportValue: "Importer un projet"
+            labelImportValue: this.props.t('import.import_project')
         }
     }
 
 
     // Function to cancel import
     cancelImport = () => {
-        this.setState({ isimporting: false, labelImportValue: "Importer un projet" })
+        this.setState({ isimporting: false, labelImportValue: this.props.t('import.import_project') })
         document.getElementById("selectFiles_1").value = ""
     }
 
     loadImportedProj = (event) => {
         if (this.state.loadedProject && this.state.loadedProject.type === "application/json") {
             // Call function to load the project
-            importProjectJsonFile(event, this.state.loadedProject, this.cancelImport)
+            importProjectJsonFile(event, this.state.loadedProject, this.cancelImport, this.props.t('errors.unable_reading_file'))
         } else {
             Swal.fire({
-                title: 'Impossible de lire ce type de fichier !',
+                title: this.props.t('errors.unable_reading_file'),
                 showCancelButton: false,
                 showConfirmButton: true,
                 confirmButtonText: 'OK',
@@ -55,23 +56,23 @@ class ImportProject extends Component {
         return (
             <div className={this.state.isimporting ? "importing_project" : "create_project_2"}  >
 
-                <div className="tooltip" data-tip="Importer un projet">
+                <div className="tooltip" data-tip={this.props.t('import.import_project')}>
                     <label className="btn btn-md btn-secondar" id="label-upload" htmlFor="selectFiles_1"> <FontAwesomeIcon icon={faUpload} /> {this.state.labelImportValue}</label>
                 </div>
 
                 <input accept="application/json" type="file" id="selectFiles_1" onChange={(e) => {
                     this.setState({ isimporting: true, loadedProject: e.target.files[0] })
-                    this.setState({ labelImportValue: "1 Fichier selectionné"})
+                    this.setState({ labelImportValue: this.props.t('files.one_selected')})
                 }} />
 
                 {
                     this.state.isimporting &&
                     <div className="import-btns">
-                        <div className="tooltip" data-tip="Valider l'importation">
+                        <div className="tooltip" data-tip={this.props.t('import.validate')}>
                             <button className="btn btn-md btn-success" disabled={!this.state.isimporting} onClick={(event) => {this.loadImportedProj(event)}}><FontAwesomeIcon icon={faCheckCircle} /></button>
                         </div>
 
-                        <div className="tooltip" data-tip="Annuler l'importation">
+                        <div className="tooltip" data-tip={this.props.t('import.cancel')}>
                             <button className="btn btn-md btn-error" disabled={!this.state.isimporting} onClick={() => this.cancelImport()}><FontAwesomeIcon icon={faCancel} /></button>
                         </div>
                     </div>
@@ -83,4 +84,4 @@ class ImportProject extends Component {
     }
 }
 
-export default withRouter(ImportProject)
+export default withTranslation()(withRouter(ImportProject));
