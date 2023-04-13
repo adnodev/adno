762ee7ec15948decb84a5a8b99dc590f08b183d9
SelligtomGit86 <thomas.delafaye@serli.com>
2023-04-13 15:19:13 +0200

    ::recycle: refactor AnnotationCards component

--- src/components/AdnoEditor/AnnotationCards/AnnotationCards.js
+++ src/components/AdnoEditor/AnnotationCards/AnnotationCards.js
@@ -37,62 +37,62 @@ class AnnotationCards extends Component {
         }
     }
 
+    // Function to move an annotation up one place
+    annoSwitchUp = (index) => {
+
+        var annos = this.props.annotations;
+
+        var annoToSwitch = annos[index - 1]
+
+        annos[index - 1] = annos[index]
+        annos[index] = annoToSwitch
+
+        insertInLS(`${this.props.match.params.id}_annotations`, JSON.stringify(annos))
+
+        this.props.updateAnnos(annos)
+    }
+
+    // Function to move an annotation down one place
+    annoSwitchDown = (index) => {
+        var annos = this.props.annotations;
+
+        var annoToSwitch = annos[index + 1]
+
+        annos[index + 1] = annos[index]
+        annos[index] = annoToSwitch
+
+        insertInLS(`${this.props.match.params.id}_annotations`, JSON.stringify(annos))
+
+        this.props.updateAnnos(annos)
+    }
+
+    // Function to delete an annotation
+    deleteAnnotation = (annotationID) => {
+
+        Swal.fire({
+            title: this.props.t('modal.ask_delete'),
+            showCancelButton: true,
+            confirmButtonText:this.props.t('modal.confirm_del_annotation'),
+            cancelButtonText: this.props.t('modal.cancel'),
+            icon: 'warning',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                var annos = this.props.annotations;
+
+                // Update the localStorage without the removed item
+                insertInLS(`${this.props.match.params.id}_annotations`, JSON.stringify(annos.filter(annotation => annotation.id != annotationID)))
+
+                Swal.fire(this.props.t('modal.del_annotation_confirmation'), '', 'success')
+                    .then((result) => {
+                        if (result.isConfirmed) {
+                            this.props.updateAnnos(annos.filter(annotation => annotation.id != annotationID))
+                        }
+                    })
+            }
+        })
+    }
+
     render() {
-        // Function to move an annotation up one place
-        const annoSwitchUp = (index) => {
-
-            var annos = this.props.annotations;
-
-            var annoToSwitch = annos[index - 1]
-
-            annos[index - 1] = annos[index]
-            annos[index] = annoToSwitch
-
-            insertInLS(`${this.props.match.params.id}_annotations`, JSON.stringify(annos))
-
-            this.props.updateAnnos(annos)
-        }
-
-        // Function to move an annotation down one place
-        const annoSwitchDown = (index) => {
-            var annos = this.props.annotations;
-
-            var annoToSwitch = annos[index + 1]
-
-            annos[index + 1] = annos[index]
-            annos[index] = annoToSwitch
-
-            insertInLS(`${this.props.match.params.id}_annotations`, JSON.stringify(annos))
-
-            this.props.updateAnnos(annos)
-        }
-
-        // Function to delete an annotation
-        const deleteAnnotation = (annotationID) => {
-
-            Swal.fire({
-                title: this.props.t('modal.ask_delete'),
-                showCancelButton: true,
-                confirmButtonText:this.props.t('modal.confirm_del_annotation'),
-                cancelButtonText: 'Annuler',
-                icon: 'warning',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    var annos = this.props.annotations;
-
-                    // Update the localStorage without the removed item
-                    insertInLS(`${this.props.match.params.id}_annotations`, JSON.stringify(annos.filter(annotation => annotation.id != annotationID)))
-
-                    Swal.fire(this.props.t('modal.del_annotation_confirmation'), '', 'success')
-                        .then((result) => {
-                            if (result.isConfirmed) {
-                                this.props.updateAnnos(annos.filter(annotation => annotation.id != annotationID))
-                            }
-                        })
-                }
-            })
-        }
-
         return (
             <div className="annotations_list">
                 {
@@ -122,9 +122,9 @@ class AnnotationCards extends Component {
                                         <button type="button"
                                             onClick={() => this.props.changeSelectedAnno(annotation)}
                                             className="btn btn-sm btn-show-more"> <FontAwesomeIcon icon={faBullseye} /></button>
-                                        {index < this.props.annotations.length - 1 ? <button className="btn btn-sm btn-outline" onClick={() => annoSwitchDown(index)}> <FontAwesomeIcon icon={faDownLong} /> </button> : <></>}
-                                        {index > 0 ? <button className="btn btn-sm" onClick={() => annoSwitchUp(index)}> <FontAwesomeIcon icon={faUpLong} /> </button> : <></>}
-                                        <button className="btn btn-sm btn-outline btn-error" onClick={() => deleteAnnotation(annotation.id)}> <FontAwesomeIcon icon={faTrashAlt} /></button>
+                                        {index < this.props.annotations.length - 1 ? <button className="btn btn-sm btn-outline" onClick={() => this.annoSwitchDown(index)}> <FontAwesomeIcon icon={faDownLong} /> </button> : <></>}
+                                        {index > 0 ? <button className="btn btn-sm" onClick={() => this.annoSwitchUp(index)}> <FontAwesomeIcon icon={faUpLong} /> </button> : <></>}
+                                        <button className="btn btn-sm btn-outline btn-error" onClick={() => this.deleteAnnotation(annotation.id)}> <FontAwesomeIcon icon={faTrashAlt} /></button>
                                     </div>
                                 </div >
                             </div >
