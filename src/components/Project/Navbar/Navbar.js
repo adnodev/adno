e584dc0c3e1c84a6461e7c3b51c905f53f83d12d
SelligtomGit86 <thomas.delafaye@serli.com>
2023-05-19 14:22:35 +0200

    Add a modal to download and share a project

--- src/components/Project/Navbar/Navbar.js
+++ src/components/Project/Navbar/Navbar.js
@@ -14,17 +14,34 @@ class Navbar extends Component {
     render() {
         return (
             <div className="navbar text-neutral-content">
-                <Link to={"/"} className="project-navbar-link project-page-title" title={this.props.t('navbar.back_home')}> 
+                <Link to={"/"} className="project-navbar-link project-page-title" title={this.props.t('navbar.back_home')}>
                     <h1>ADNO</h1>
                 </Link>
-                <Link to={"/"} className="project-navbar-link" title={this.props.t('navbar.back_home')}> <FontAwesomeIcon icon={faHome} size="lg"/> </Link>
+                <Link to={"/"} className="project-navbar-link" title={this.props.t('navbar.back_home')}> <FontAwesomeIcon icon={faHome} size="lg" /> </Link>
 
-                {
-                    this.props.selectedProject && this.props.selectedProject.id &&
-                    <a id={"download_btn_" + this.props.selectedProject.id} href={createExportProjectJsonFile(this.props.selectedProject.id)} download={this.props.selectedProject.title + ".json"} className="project-navbar-link" title={this.props.t('navbar.download_project')}> <FontAwesomeIcon icon={faDownload} size="lg"/> </a>
-                }
+                {/* // Display a modal to download and share the current project */}
+                <label htmlFor="my-modal" style={{ "background": "none", "border": "none" }} className="btn project-navbar-link"><FontAwesomeIcon icon={faDownload} size="lg" /> </label>
 
-                <button onClick={() => this.props.showProjectMetadatas()} className="project-navbar-link" title={this.props.editMode ? this.props.t('navbar.edit_project') : this.props.t('navbar.show_metadatas')}><FontAwesomeIcon icon={this.props.editMode ? faFilePen : faFile} size="lg"/></button>
+                <input type="checkbox" id="my-modal" className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box" style={{ "color": "initial" }}>
+                        <h3 className="font-bold text-lg">Partage du projet</h3>
+                        <p className="py-4">Afin de pouvoir travailler de manière collaborative, vous devez télécharger le projet puis le partager à vos collaborateurs via un service comme IPFS, Google Drive ou WeTransfer.</p>
+                        <p className="py-4">Ainsi, vos collaborateurs pourront importer le projet de leur côté afin de profiter de vos changements.</p>
+                        <p className="py-4">Pour plus d'informations, rendez-vous sur la <a className="adno-link" href="https://adno.app/fr/docs/prologue/quick-start/" target="_blank">documentation officielle d'ADNO</a></p>
+                        <div className="modal-action">
+                            <label htmlFor="my-modal" className="btn">Annuler</label>
+                            <label className="btn btn-success">
+                                {
+                                    this.props.selectedProject && this.props.selectedProject.id &&
+                                    <a id={"download_btn_" + this.props.selectedProject.id} href={createExportProjectJsonFile(this.props.selectedProject.id)} download={this.props.selectedProject.title + ".json"} title={this.props.t('navbar.download_project')}> <FontAwesomeIcon icon={faDownload} size="lg" /> </a>
+                                }
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <button onClick={() => this.props.showProjectMetadatas()} className="project-navbar-link" title={this.props.editMode ? this.props.t('navbar.edit_project') : this.props.t('navbar.show_metadatas')}><FontAwesomeIcon icon={this.props.editMode ? faFilePen : faFile} size="lg" /></button>
 
                 <strong>{this.props.selectedProject.title} {this.props.selectedProject.autor && `(${this.props.selectedProject.autor})`}</strong>
 
@@ -34,7 +51,7 @@ class Navbar extends Component {
 
                         {
                             !this.props.editMode &&
-                            <button onClick={() => this.props.showEditorSettings()} className="btn btn-md"><FontAwesomeIcon icon={faGear} size="lg"/></button>
+                            <button onClick={() => this.props.showEditorSettings()} className="btn btn-md"><FontAwesomeIcon icon={faGear} size="lg" /></button>
                         }
 
 
