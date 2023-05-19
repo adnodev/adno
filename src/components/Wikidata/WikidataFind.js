7f4c5db2f9fb6e2319c35d6a3690954e35f409b6
SelligtomGit86 <thomas.delafaye@serli.com>
2023-05-19 17:44:45 +0200

    Add WikidataFind component to find something from wikidata and get the data with the image url

---
+++ src/components/Wikidata/WikidataFind.js
@@ -0,0 +1,61 @@
+import WBK from "wikibase-sdk"
+
+export function searchOnWikidata(textQuery) {
+
+    wbk = WBK({
+        instance: 'https://www.wikidata.org',
+        sparqlEndpoint: 'https://query.wikidata.org/sparql'
+    })
+    const language = "fr";
+    const limit = 10;
+    const format = "json"
+
+    const url = wbk.searchEntities({
+        search: textQuery,
+        language,
+        limit,
+        format,
+    })
+    fetch(url)
+        .then(resp => {
+            if (resp.status === 200) {
+                resp.json()
+                    .then(data => {
+
+                        data.search.forEach(element => {
+
+                            console.log(element);
+                            console.log(element.label);
+                            console.log(element.description);
+
+                            let ent = this.wbk.getEntities({ ids: element.id })
+
+                            // Next, we have to find the attached image
+                            fetch(ent)
+                                .then(resEnt => resEnt.json())
+                                .then(wikiEntity => {
+                                    console.log("wikiEntity", wikiEntity.entities[element.id]);
+
+
+                                    let images = wikiEntity.entities[element.id] && wikiEntity.entities[element.id].claims["P18"]
+
+
+                                    if(images){
+                                        let imgUrl = `https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/${images[0].mainsnak.datavalue.value}&width=100`
+
+                                        console.log("imgUrl", imgUrl);
+    
+                                    }
+
+                                 
+                                })
+                        });
+
+
+                    })
+
+            } else {
+                console.error("Impossible de trouver l'oeuvre");
+            }
+        })
+}
