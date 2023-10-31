d21aa11b23831b34a58a3603dcdcfb8dbe3d3c73
SelligtomGit86 <thomas.delafaye@serli.com>
2023-10-31 12:57:10 +0100

    Parse wikidata link on viewer

--- src/components/AdnoMarkdown/AdnoMdViewer.js
+++ src/components/AdnoMarkdown/AdnoMdViewer.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import ReactMarkdown from 'react-markdown'
+import WBK from "wikibase-sdk"
 
 import "./AdnoMarkdown.css";
 
@@ -7,15 +8,63 @@ class AdnoMdViewer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            annos: "",
+            isLoaded: false
         }
     }
 
+    componentDidMount() {
+        this.getAnnoBody()
+        this.setState({ isLoaded: true })
+    }
+
     getAnnoBody = () => {
+        const wbk = WBK({
+            instance: 'https://www.wikidata.org',
+            sparqlEndpoint: 'https://query.wikidata.org/sparql'
+        })
+
         if (Array.isArray(this.props.selectedAnnotation.body) && this.props.selectedAnnotation.body.length > 0) {
-            return this.props.selectedAnnotation.body.filter(annobody => annobody.type === "TextualBody" && annobody.purpose === "commenting")[0] ? this.props.selectedAnnotation.body.filter(annobody => annobody.type === "TextualBody" && annobody.purpose === "commenting")[0].value : ""
+            var annoMdBody = this.props.selectedAnnotation.body.filter(annobody => annobody.type === "TextualBody" && annobody.purpose === "commenting")[0] ? this.props.selectedAnnotation.body.filter(annobody => annobody.type === "TextualBody" && annobody.purpose === "commenting")[0].value : ""
+
+            var finalBody = ""
+
+            annoMdBody.split("\n\n").forEach(async (line) => {
+
+                if (line.match("https?:\/\/www.wikidata.org\/wiki\/[a-zA-Z0-9]*")) {
+                    const element = line.match("https?:\/\/www.wikidata.org\/wiki\/[a-zA-Z0-9]*")[0];
+
+                    const wikiID = element.replace('https://www.wikidata.org/wiki/', '')
+
+                    const url = wbk.getEntities({
+                        ids: [wikiID],
+                        language: ['fr']
+                    })
+
+                    const { entities } = await fetch(url).then(res => res.json())
+
+                    const wikiName = `[${entities[wikiID].labels.fr.value}](${element})`;
+                    const wikiDesc = entities[wikiID].descriptions.fr.value;
+
+                    let images = entities[wikiID] && entities[wikiID].claims["P18"]
+
+                    finalBody += "\n\n" + wikiName + "\n\n"
+                    finalBody += wikiDesc + "\n\n"
+
+                    if (images) {
+                        const imgUrl = `https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/${images[0].mainsnak.datavalue.value}&width=200`
+                        finalBody += `![${wikiName}](${new URL(imgUrl)})` + "\n\n"
+                    }
+                    this.setState({ annos: finalBody })
+
+                } else {
+                    finalBody += line
+                    this.setState({ annos: finalBody })
+                }
+            })
+
         } else {
-            return ""
+            this.setState({ annos: "" })
         }
     }
 
@@ -32,7 +81,10 @@ class AdnoMdViewer extends Component {
                             </div>
                             <div className="card-body over-hidden">
                                 <div className="markdown-body">
-                                    <ReactMarkdown children={this.getAnnoBody()} />
+                                    {
+                                        this.state.isLoaded && this.state.annos &&
+                                        <ReactMarkdown children={this.state.annos} />
+                                    }
                                 </div>
                             </div>
 
