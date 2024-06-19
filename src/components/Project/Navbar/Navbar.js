44a7ac7a2429230d875910ddfa54480015978d12
zwiterrion <etienne.anne@icloud.com>
2024-06-19 22:05:34 +0200

    refacto

--- src/components/Project/Navbar/Navbar.js
+++ src/components/Project/Navbar/Navbar.js
@@ -60,7 +60,13 @@ class Navbar extends Component {
                     </button>
                 </div>
 
-                <strong>{this.props.selectedProject.title.length <= 81 ? this.props.selectedProject.title : `${this.props.selectedProject.title.substring(0,80)}…`}<br /> {(this.props.settings?.tags || []).map(tag => ` #${tag} `)} {this.props.selectedProject.creator && ` [ ${this.props.selectedProject.creator} ]`}</strong>
+                <strong style={{
+                    maxWidth: 400,
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    display: 'block'
+                }}>{this.props.selectedProject.title}<br /> {(this.props.settings?.tags || []).map(tag => ` #${tag} `)} {this.props.selectedProject.creator && ` [ ${this.props.selectedProject.creator} ]`}</strong>
 
                 {
                     process.env.ADNO_MODE === "FULL" &&
@@ -72,7 +78,7 @@ class Navbar extends Component {
                         }
 
 
-                        <div className="tooltip tooltip-bottom z-50" data-tip={this.props.editMode ? this.props.t('navbar.edit_mode') : this.props.t('navbar.view_mode') }>
+                        <div className="tooltip tooltip-bottom z-50" data-tip={this.props.editMode ? this.props.t('navbar.edit_mode') : this.props.t('navbar.view_mode')}>
                             <label className="cursor-pointer label label-toggle">
                                 <label>{this.props.editMode ? this.props.t('navbar.edit_mode') : this.props.t('navbar.view_mode')}</label>
                                 <input type="checkbox" className="toggle toggle-lg toggle-success" value={this.props.editMode}
