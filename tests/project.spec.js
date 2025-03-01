113da0b6976992851120e8ec837af1e82ebe3729
zwiterrion <etienne.anne@icloud.com>
2025-03-01 08:39:40 +0100

    add test about iiif export

--- tests/project.spec.js
+++ tests/project.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+import path from 'path'
 
 test('should import https://bastaire.msh.uca.fr/iiif/3/10243/manifest as project', async ({ page }) => {
   await page.goto('http://localhost:1234/#/');
@@ -26,3 +27,19 @@ test('should https://static.emf.fr/adno/bsg/annotations.json detect as adno proj
   await page.getByRole('button', { name: 'OK' }).click();
   await expect(page.locator('.a9s-annotationlayer')).toBeVisible();
 });
+
+test('should load IIFF export files successfully', async ({ page }) => {
+  await page.goto('http://localhost:1234/#/');
+
+  const fileChooserPromise = page.waitForEvent('filechooser');
+  await page.getByText('Import a project').click();
+  const fileChooser = await fileChooserPromise;
+  await fileChooser.setFiles(path.join(__dirname, 'assets', 'iiif_export.json'));
+
+  await page.getByRole('button').filter({ hasText: /^$/ }).first().click();
+  await expect(page.locator('g:nth-child(2) > .a9s-inner')).toBeVisible();
+  await expect(page.locator('.a9s-inner').first()).toBeVisible();
+  await expect(page.locator('g:nth-child(4) > .a9s-inner')).toBeVisible();
+  await expect(page.locator('rect').nth(4)).toBeVisible();
+  await expect(page.locator('rect').nth(2)).toBeVisible();
+})
